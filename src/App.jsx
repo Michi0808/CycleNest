461127d583e4553bd044a2f0283a6efe80e96bd7
Michi <jagar008@gmail.com>
2025-12-27 00:02:06 +0100

    feat: confirm adding events in Google calendar from React client

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,66 @@ import { Sidebar, Menu, MenuItem, Submenu, Logo } from 'react-mui-sidebar';
 import AccessAlarms from '@mui/icons-material/AccessAlarms';
 import CottageOutlinedIcon from '@mui/icons-material/CottageOutlined';
 import { Link } from 'react-router-dom';
+import { gapi } from 'gapi-script';
 
 function App() {
+  const calendarID = import.meta.env.VITE_CALENDAR_ID;
+  const accessToken = import.meta.env.VITE_GOOGLE_ACCESS_TOKEN;
+
+  const event = {
+    summary: 'Hello World',
+    location: '',
+    start: {
+      dateTime: '2025-12-27T09:00:00-07:00',
+      timeZone: 'America/Los_Angeles',
+    },
+    end: {
+      dateTime: '2025-12-27T17:00:00-07:00',
+      timeZone: 'America/Los_Angeles',
+    },
+    recurrence: ['RRULE:FREQ=DAILY;COUNT=2'],
+    attendees: [],
+    reminders: {
+      useDefault: false,
+      overrides: [
+        { method: 'email', minutes: 24 * 60 },
+        { method: 'popup', minutes: 10 },
+      ],
+    },
+  };
+
+  const addEvent = (calendarID, event) => {
+    function initiate() {
+      gapi.client
+        .request({
+          path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
+          method: 'POST',
+          body: event,
+          headers: {
+            'Content-type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then(
+          (response) => {
+            return [true, response];
+          },
+          function (err) {
+            console.log(err);
+            return [false, err];
+          }
+        );
+    }
+    gapi.load('client', initiate);
+  };
+
   return (
     <>
       <div className="flex h-screen">
+        {/* API Test */}
+        <button type="button" onClick={() => addEvent(calendarID, event)}>
+          Add Event
+        </button>
         <Sidebar width={'270px'}>
           <Logo
             component={Link}
