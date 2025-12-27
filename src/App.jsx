02f1dfe946ed25f55964b582ed7ec8c2033046f1
Michi <jagar008@gmail.com>
2025-12-27 23:20:32 +0100

    refactor: extract Sidebar into separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import './App.css';
 import Calendar from './components/calendar/calendar';
-import { Sidebar, Menu, MenuItem, Submenu, Logo } from 'react-mui-sidebar';
-import AccessAlarms from '@mui/icons-material/AccessAlarms';
-import CottageOutlinedIcon from '@mui/icons-material/CottageOutlined';
-import { Link } from 'react-router-dom';
 import { gapi } from 'gapi-script';
+import Menubar from './components/Menubar/Menubar';
 
 function App() {
   const calendarID = import.meta.env.VITE_CALENDAR_ID;
@@ -64,57 +61,7 @@ function App() {
         <button type="button" onClick={() => addEvent(calendarID, event)}>
           Add Event
         </button>
-        <Sidebar width={'270px'}>
-          <Logo
-            component={Link}
-            href="/"
-            img="https://adminmart.com/wp-content/uploads/2024/03/logo-admin-mart-news.png"
-          >
-            AdminMart
-          </Logo>
-          <Menu subHeading="HOME">
-            <MenuItem
-              icon={<CottageOutlinedIcon />}
-              component={Link}
-              link="/tes"
-              badge={true}
-              isSelected={true}
-            >
-              {' '}
-              {/* Set badge to boolean true */}
-              Modern
-            </MenuItem>
-            <MenuItem icon={<AccessAlarms />} component={Link} link="/test">
-              eCommerce
-            </MenuItem>
-            <MenuItem component={Link} link="/ana">
-              Analytical
-            </MenuItem>
-          </Menu>
-          <Menu subHeading="APPS">
-            <MenuItem>Chat</MenuItem>
-            <MenuItem>Calendar</MenuItem>
-          </Menu>
-          <Menu subHeading="OTHERS">
-            <Submenu title="Menu Level">
-              <MenuItem>Post</MenuItem>
-              <MenuItem>Details</MenuItem>
-              <Submenu title="Level 2">
-                <MenuItem>new</MenuItem>
-                <MenuItem>Hello</MenuItem>
-              </Submenu>
-            </Submenu>
-
-            <MenuItem>Chip</MenuItem>
-            <MenuItem
-              target="_blank"
-              component={Link}
-              link="https://google.com"
-            >
-              External Link
-            </MenuItem>
-          </Menu>
-        </Sidebar>
+        <Menubar />
         <Calendar />
       </div>
     </>
---
+++ src/components/Menubar/Menubar.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Sidebar, Menu, MenuItem, Submenu, Logo } from 'react-mui-sidebar';
+import AccessAlarms from '@mui/icons-material/AccessAlarms';
+import CottageOutlinedIcon from '@mui/icons-material/CottageOutlined';
+import { Link } from 'react-router-dom';
+
+export default function Menubar() {
+  return (
+    <Sidebar width={'270px'}>
+      <Logo
+        component={Link}
+        href="/"
+        img="https://adminmart.com/wp-content/uploads/2024/03/logo-admin-mart-news.png"
+      >
+        AdminMart
+      </Logo>
+      <Menu subHeading="HOME">
+        <MenuItem
+          icon={<CottageOutlinedIcon />}
+          component={Link}
+          link="/tes"
+          badge={true}
+          isSelected={true}
+        >
+          {' '}
+          {/* Set badge to boolean true */}
+          Modern
+        </MenuItem>
+        <MenuItem icon={<AccessAlarms />} component={Link} link="/test">
+          eCommerce
+        </MenuItem>
+        <MenuItem component={Link} link="/ana">
+          Analytical
+        </MenuItem>
+      </Menu>
+      <Menu subHeading="APPS">
+        <MenuItem>Chat</MenuItem>
+        <MenuItem>Calendar</MenuItem>
+      </Menu>
+      <Menu subHeading="OTHERS">
+        <Submenu title="Menu Level">
+          <MenuItem>Post</MenuItem>
+          <MenuItem>Details</MenuItem>
+          <Submenu title="Level 2">
+            <MenuItem>new</MenuItem>
+            <MenuItem>Hello</MenuItem>
+          </Submenu>
+        </Submenu>
+
+        <MenuItem>Chip</MenuItem>
+        <MenuItem target="_blank" component={Link} link="https://google.com">
+          External Link
+        </MenuItem>
+      </Menu>
+    </Sidebar>
+  );
+}
